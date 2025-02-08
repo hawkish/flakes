be990ca0917af3e9ed468ee58cfc1b1fbafbeb5d
hawkish <mortenhogh@gmail.com>
2025-02-08 17:32:31 +0100

    Extended nodejs

--- nodejs/flake.nix
+++ nodejs/flake.nix
@@ -15,13 +15,30 @@
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = [
+          buildInputs = [
             pkgs.zsh
-            pkgs.nodejs_22
+            pkgs.nodejs
+
+            # You can set the major version of Node.js to a specific one instead
+            # of the default version
+            # pkgs.nodejs-22_x
+
+            # Comment out one of these to use an alternative package manager.
+            # pkgs.yarn
+            # pkgs.pnpm
+            # pkgs.bun
+
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
           ];
+
           shellHook = ''
             export SHELL=$(which zsh)
-            exec zsh
+            exec zsh 
+            if [ ! -d "node_modules" ]; then
+                echo "Installing npm dependencies..."
+                npm install
+            fi
           '';
         };
       }
