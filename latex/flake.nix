3a6b8fbbee965c68bcb5a0c7005dada176869173
hawkish <mortenhogh@gmail.com>
2025-02-23 14:58:45 +0100

    Added Latex

---
+++ latex/flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "LaTeX Document based on https://flyx.org/nix-flakes-latex/";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    with flake-utils.lib;
+    eachSystem allSystems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-basic
+            latexxmk
+            pgf
+            nicematrix
+            fontspec
+            ;
+        };
+
+      in
+      rec {
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "latex-demo-document";
+            src = self;
+            buildInputs = [
+              pkgs.coreutils
+              pkgs.fira-code
+              tex
+            ];
+            phases = [
+              "unpackPhase"
+              "buildPhase"
+              "installPhase"
+            ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                  OSFONTDIR=${pkgs.fira-code}/share/fonts \
+                latexmk -interaction=nonstopmode -pdf -lualatex \
+                document.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp document.pdf $out/
+            '';
+          };
+        };
+
+        defaultPackage = packages.document;
+      }
+    );
+}
