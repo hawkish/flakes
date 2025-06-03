f8d130e7d542a6dcd706fb716b896cbd96883aa2
hawkish <mortenhogh@gmail.com>
2025-06-03 19:18:34 +0200

    Added claude

--- nodejs/flake.nix
+++ nodejs/flake.nix
@@ -30,6 +30,7 @@
 
             pkgs.nodePackages.typescript
             pkgs.nodePackages.typescript-language-server
+            pkgs.nodePackages.claude-code
           ];
 
           shellHook = ''
